194925a31fb7996e745cfeb1cb889795c09dd08f
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-07-28 17:41:11 -0600

    add my own tab

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -36,6 +36,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="my_profile"
+        options={{
+            title: 'Profile',
+            tabBarIcon: ({color, focused}) => (
+                <Ionicons name={ focused ? 'person-sharp' : 'person-outline'} color={color} size={24} />
+        ),
+        }}
+        />
     </Tabs>
   );
 }
