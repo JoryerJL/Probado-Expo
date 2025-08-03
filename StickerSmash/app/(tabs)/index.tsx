a40ae863357941e1c6d9246707bb65caffe40de1
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-08-02 23:29:33 -0600

    Chapter 4: Use an image picker

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -3,17 +3,33 @@ import { Link, Stack } from 'expo-router';
 import { Image } from 'expo-image';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from 'react';
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled){
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert("You didn't select any image.");
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme='primary' label="Choose a photo" />
+        <Button theme='primary' label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
